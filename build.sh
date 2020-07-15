2bfa75e59e67be432650506b8d69b663e069ed87
kei <kei@null>
2020-07-15 15:19:29 +0200

    Added gcc fallback and few fixes

--- build.sh
+++ build.sh
@@ -11,12 +11,26 @@ IODINE_MAKE_FLAGS="-j`nproc`"
 IODINE_SIGNING="n"
 IODINE_SIGNING_KEY="certs/kernel_key.pem"
 
+#	If the compiler path is wrong, fall back to GCC
+
+if ! type $IODINE_COMPILER >/dev/null 2>&1; then
+	echo "  - No compiler found for $IODINE_COMPILER, falling back to GCC"
+
+	if ! type gcc >/dev/null 2>&1; then
+		echo "  - Couldn't find GCC, is it installed?"
+	else
+		IODINE_COMPILER="gcc"
+	fi
+fi
+
+#	Clone the chosen branch if doesn't exists
+
 if [ ! -d "linux" ]; then
 	echo " [*] Fetching kernel $IODINE_LINUX_VERSION"
 
 	git clone -b $IODINE_LINUX_VERSION --single-branch --depth 1 $IODINE_LINUX_REPOSITORY
 else
-	echo " [-] Kernel folder found"
+	echo " [*] Kernel folder found"
 fi
 
 echo " [*] Applying patches"
@@ -24,6 +38,8 @@ echo " [*] Applying patches"
 for patch in patches/*.patch; do
 	patch -sNp1 -dlinux < $patch
 done
+
+cp "configs/$IODINE_LINUX_VERSION" linux/.config
 
 cd linux
 
@@ -35,8 +51,6 @@ if [ $IODINE_SIGNING = "y" ]; then
 else
 	scripts/config --disable CONFIG_MODULE_SIG_ALL
 fi
-
-cp "../configs/$IODINE_LINUX_VERSION" .config
 
 echo " [*] Building (using $IODINE_COMPILER $IODINE_MAKE_FLAGS)"
 
